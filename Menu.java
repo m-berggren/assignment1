8d1c4c9118e30993658afe0486579029005cac06
P4FFos <143185395+P4FFos@users.noreply.github.com>
2023-09-08 14:13:52 +0200

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -4,16 +4,20 @@ public class Menu {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int userOptionInMenu = 0;
+        int userOptionInMenu = 0; // Assigned value of the variable from the beginning
 
         do {
+            // the visual part of the menu that is printed in the console
             String systemMenu = "\n Welcome to the menu. Choose one of the options below: \n 1. Register new scores for students. \n 2. Print all registered grades. \n 3. Calculate the mean of the entered scores. \n 4. Find the two highest and two lowest scores. \n 5. Find the highest score and its position. \n 6. Collect hashtags from a post. \n 7. To exit. \n";
             System.out.println(systemMenu);
+
+            // ask user to assign the value of variable to call the tasks in menu later
             System.out.println("Type your option:");
             userOptionInMenu = input.nextInt();
             input.nextLine();
 
             if (userOptionInMenu > 7 || userOptionInMenu < 0 ){
+                // console print error and ask to type another number, while input number is not in the range
                 System.out.println("Error - Invalid value. Please type between 1 and 7");
             }
             if (userOptionInMenu == 1) {
@@ -41,9 +45,13 @@ public class Menu {
 
             }
             else if (userOptionInMenu == 7) {
+                // console print thankful message after user assigned value of 7 to the variable
                 System.out.println("Thank you for using our grading system. Have a nice day! ");
             }
-        } while (userOptionInMenu != 7);
-        input.close();
+
+        } while (userOptionInMenu != 7); // do loop works while value of the variable is not equal to 7
+
+
+        input.close(); // to close the scanner
     }
 }
--- calculatingMean.java
+++ calculatingMean.java
@@ -5,30 +5,10 @@ public class calculatingMean {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] scoreArray = new int[7];
-        String[] suffixArray = {"st", "nd", "rd", "th", "th", "th", "th"};
-        int i = 0;
-
-        System.out.println("Please enter 7 integer scores.");
-
-        while (i < suffixArray.length) {
-            System.out.printf("Enter the score for the %d%s student ", i+1, suffixArray[i]);
-
-            int score = input.nextInt();
-            input.nextLine();
-
-            if(score > 100 || score < 0) {
-                System.out.println("Error - Input out of bound. Score can only be between 0 and 100.");
-            } else {
-                scoreArray[i] = score;
-                i++;
-            }
-        }
-
-        double meanOfPoints = (scoreArray[0] + scoreArray[1] + scoreArray[2] + scoreArray[3] + scoreArray[4] + scoreArray[5] + scoreArray[6])/scoreArray.length;
+        double meanOfPoints;
+        meanOfPoints = (scoreArray[0] + scoreArray[1] + scoreArray[2] + scoreArray[3] + scoreArray[4] + scoreArray[5] + scoreArray[6])/scoreArray.length;
         Locale.setDefault(Locale.ENGLISH);
-        System.out.printf("The mean of the numbers is: %.2f " ,meanOfPoints);
+        System.out.printf("The mean of the numbers is: %.2f " , meanOfPoints);
 
-        input.close();
     }
 }
