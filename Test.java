395a05a16d79ceb058870a3b1a69513627ca194b
Marcus Berggren <89553636+marcusberggren@users.noreply.github.com>
2023-09-10 22:51:55 +0200

    Delete Test.java

--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Input a number:");
-        int value = IOScanner.readInt();
-        System.out.print(value);
-    }
-}
