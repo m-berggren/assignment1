eb75f076ba9f73978c907911d60bc869b10353f1
Marcus Berggren <89553636+marcusberggren@users.noreply.github.com>
2023-09-12 13:34:48 +0200

    Add files via upload

---
+++ IOScanner.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IOScanner {
+    private final Scanner scanner;
+    public IOScanner() {
+        this.scanner = new Scanner(System.in);
+    }
+    // Methods for reading int, string, double.
+
+    public int readInt() {
+        int num = scanner.nextInt();
+        scanner.nextLine();
+        return num;
+    }
+    public double readDouble() {
+        double dbl = scanner.nextDouble();
+        scanner.nextLine();
+        return dbl;
+    }
+    public String readString() {
+        return scanner.nextLine();
+    }
+    public void close() {
+        scanner.close();
+    }
+}
