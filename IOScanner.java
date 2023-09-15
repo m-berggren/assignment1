0fee93bc2dfd4fa2fd3cad6ed707c5bb3575f466
Marcus Berggren <89553636+m-berggren@users.noreply.github.com>
2023-09-15 12:28:12 +0200

    Update IOScanner.java

--- IOScanner.java
+++ IOScanner.java
@@ -1,24 +1,24 @@
 
 import java.util.Scanner;
 public class IOScanner {
-    static Scanner scannerObj = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     static int readInt() {
-        int intNumber = scannerObj.nextInt();
-        scannerObj.nextLine();
+        int intNumber = scanner.nextInt();
+        scanner.nextLine();
         return intNumber;
     }
     static int readIntWithMessage(String messageFromString) {
         System.out.println(messageFromString);
-        int intNumber = scannerObj.nextInt();
-        scannerObj.nextLine();
+        int intNumber = scanner.nextInt();
+        scanner.nextLine();
         return intNumber;
     }
     static String readStringWithMessage(String messageFromString) {
         System.out.println(messageFromString);
-        return scannerObj.nextLine();
+        return scanner.nextLine();
     }
     static void close() {
-        scannerObj.close();
+        scanner.close();
     }
 }
